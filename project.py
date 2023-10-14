d403b333d95c7c6b6a948e9a10d3c25c6b4ec766
alltaro <romeomath07@gmail.com>
2023-10-15 01:08:50 +0200

    Version finale du projet

--- project.py
+++ project.py
@@ -5,10 +5,11 @@ import turtle
 #/////////////////////////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////////////////////////
 divide_orientation_circle = 1
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+colors = ["#c72e20", "#d1992a", "#b7bf21", "#80bf28", "#22b385", "#2095c7", "#8f20c7", "#b01c72"]
+#    rouge      orange            jaoune     vert     bleu foncé    violet      
 longueur = 23
-cran = [4, 13, 25, 40,0]
-valeur = [7.6, 5, 2, 0.78, 0.50]
+cran = [4, 13, 25,30,40,50,60,0]
+valeur = [7.2, 4.8,3 ,2, 0.90,0.70,0.55,0.47]
 pos_x = []
 pos_y = []
 fenetre = t.Screen()
@@ -47,7 +48,7 @@ def fleure6(taille_multiplicateur):
     x = taille_multiplicateur
     t.pensize(2)
     taille = randint(30, 70)
-    t.color("red","orange")
+    t.color(colors[1],colors[2])
     for _ in range(20):
         t.begin_fill()
         t.circle(100*x, 45)
@@ -57,49 +58,28 @@ def fleure6(taille_multiplicateur):
         t.left(18)
         
 
-
-def draw_tulip(size):
-    turtle.forward(size)
-    turtle.left(90)
-    turtle.forward(size)
-    turtle.left(90)
-    turtle.forward(size)
-    turtle.left(90)
-    turtle.forward(size)
-    turtle.left(90)
-
-def draw_flower(size):
-    for _ in range(4):
-        draw_tulip(size)
-        turtle.right(90)
-
-def tulipe( size):
-    for _ in range(4):
-        draw_flower(size)
-        turtle.right(360 / 10)
-
 def rosace(n, i, pos_x_y):
     t.tracer(0)
     if n == 1:
         for i in range(15):
-            t.pencolor(colors[i%6])
-            t.fillcolor(colors[i%6])
+            t.pencolor(colors[i%8])
+            t.fillcolor(colors[i%8])
             carré(longueur*2)
             carré(longueur/3*2*2)
             t.right(360/15)
     elif n == 2:
-        taille = uniform(30, 70)
+        taille = uniform(30, 60)
         for _ in range(9):
             begin_fill()
-            circle(taille, 90)  # Utilisez random.uniform pour varier la taille
+            circle(taille, 90) 
             left(90)
-            circle(taille, 90)  # Utilisez random.uniform pour varier la taille
+            circle(taille, 90)
             end_fill()
             left(10)
     elif n == 3:
         for i in range(20):
-            t.pencolor(colors[i%6])
-            t.fillcolor(colors[i%6])
+            t.pencolor(colors[i%8])
+            t.fillcolor(colors[i%8])
             t.begin_fill()
             losange(longueur*1.5)
             t.right(360/20)
@@ -107,31 +87,34 @@ def rosace(n, i, pos_x_y):
     elif n == 4 :
         t.pensize(1)
         for i in range(8):
-            t.fillcolor(colors[i%6])
+            t.fillcolor(colors[i%8])
             t.begin_fill()
             t.circle(30)
             t.end_fill()
-            t.fillcolor("white")
+            t.fillcolor("#ffffff")
             t.begin_fill()
             t.circle(26)
             t.end_fill()
-            t.fillcolor(colors[i%6])
+            t.fillcolor(colors[i%8])
             t.begin_fill()
             t.circle(20)
             t.end_fill()
-            t.fillcolor("white")
+            t.fillcolor("#ffffff")
             t.begin_fill()
             t.circle(16)
             t.end_fill()
             t.rt(45)
     elif n == 5:
         t.begin_fill()
+        t.pensize(1)
         for _ in range(45):
-            t.forward(100)
+            t.forward(100/1.5)
             t.left(170)
-            t.forward(100)
+            t.forward(100/1.5)
         t.end_fill()
     elif n == 6:
+        if randint(1,2) == 2:
+            t.color(colors[2],colors[3])
         print(pos_x_y[i][0],pos_x_y[i][1])
         t.setheading(0)
         aller_à(pos_x_y[i][0],pos_x_y[i][1])
@@ -141,12 +124,10 @@ def rosace(n, i, pos_x_y):
         aller_à(pos_x_y[i][0],pos_x_y[i][1]+8.5)
         fleure6(0.5)
         t.circle(15)
-    elif n == 7:
-        tulipe(50)
     t.tracer(1)
  
 def dessiner_tige():
-    tige_orientation = uniform(-70,70)
+    tige_orientation = uniform(-80,80)
     taille_tige = uniform(25, 38)
     if tige_orientation < 0:
         t.left(-tige_orientation)
@@ -164,7 +145,7 @@ def dessiner_tige():
                 break
         t.circle(150*divide_orientation_circle*1.5*2*2,taille_tige/divide_orientation_circle)
     
-def organisation_des_points(x, y):
+def organisation_des_points_croissant(x, y):
     print(zip(x, y))
     points = list(zip(x, y))
     print(points)
@@ -188,14 +169,13 @@ def bouquet():
         retour_au_départ()
     for i in range(nombre_fleurs):
         t.setheading(90)
-        t.color(colors[i%6],colors[(i+1)%6])
-        pos_x_y = organisation_des_points(pos_x,pos_y)
+        t.color(colors[i%8],colors[(i+1)%8])
+        pos_x_y = organisation_des_points_croissant(pos_x,pos_y)
         aller_à(pos_x_y[i][0],pos_x_y[i][1])
         t.pensize(randint(2,3))
-        rosace(1+((i+fleur_de_départ)%7),i, pos_x_y)
+        rosace(1+((i+fleur_de_départ)%6),i, pos_x_y)
         retour_au_départ()
         
 
 bouquet()
 done()
-
